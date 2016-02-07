a445622a86381248fba10b8bae0a18655f9e6a15
ajlopez <webmaster@ajlopez.com>
2016-02-07 14:30:30 -0300

    compile name expression

--- test/expressions.js
+++ test/expressions.js
@@ -1,5 +1,6 @@
 
 var x = require('../lib/expressions');
+var contexts = require('../lib/contexts');
 
 exports['integer constant expression'] = function (test) {
     var result = x.constant(42);
@@ -13,5 +14,29 @@ exports['string constant expression'] = function (test) {
     
     test.equal(result.evaluate(), "foo");
     test.equal(result.compile(), '"foo"');
+};
+
+exports['false name expression'] = function (test) {
+    var result = x.name("false");
+    
+    test.equal(result.evaluate(), false);
+    test.equal(result.compile(), "false");
+};
+
+exports['true name expression'] = function (test) {
+    var result = x.name("true");
+    
+    test.equal(result.evaluate(), true);
+    test.equal(result.compile(), "true");
+};
+
+exports['name expression'] = function (test) {
+    var result = x.name("foo");
+    var ctx = contexts.createContext();
+    
+    ctx.setLocalValue('foo', 42);
+    
+    test.equal(result.evaluate(ctx), 42);
+    test.equal(result.compile(), "foo");
 };
 
