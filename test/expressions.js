811107b14d6199729b005ad6eb14e342f01579e5
ajlopez <webmaster@ajlopez.com>
2016-02-07 14:18:31 -0300

    compile constant expression

--- test/expressions.js
+++ test/expressions.js
@@ -5,11 +5,13 @@ exports['integer constant expression'] = function (test) {
     var result = x.constant(42);
     
     test.equal(result.evaluate(), 42);
+    test.equal(result.compile(), "42");
 };
 
 exports['string constant expression'] = function (test) {
     var result = x.constant("foo");
     
     test.equal(result.evaluate(), "foo");
+    test.equal(result.compile(), '"foo"');
 };
 
