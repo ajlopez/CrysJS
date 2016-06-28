213a8a725ff83a7ecea2730bfb6f72a9b21fc058
ajlopez <webmaster@ajlopez.com>
2016-06-28 09:31:44 -0300

    compile binary and expression

--- test/compile.js
+++ test/compile.js
@@ -88,6 +88,10 @@ exports['compile logical or expression'] = function (test) {
     compile('1 || 2', '1 || 2;', test);
 };
 
+exports['compile binary and expression'] = function (test) {
+    compile('1 & 2', '1 & 2;', test);
+};
+
 exports['compile comparison expressions'] = function (test) {
     compile('1<2', '1 < 2;', test);
     compile('1>2', '1 > 2;', test);
